dcaa48940c393c48cd3f9d41ff86c123d4524d6c
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2019-10-08 23:16:28 +0200

    add logger and add main program to run crawler. Add requests_cache library

--- soup_scraper.py
+++ soup_scraper.py
@@ -1,78 +1,138 @@
 import requests
 import re
+import requests_cache
+import time
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+requests_cache.install_cache('weather_archive_cach', expire_after=36000)
 
+links_list_level_0 = []
+links_list_level_1 = []
+links_list_level_2 = []
+links_total = []
+counter = 0
 
-
-def get_url():
-    url = input("What website would you like to parse? ")
-    return url
-def parse_website(url):
-    try:
-        content = requests.get(url)
-
-        soup = BeautifulSoup(content.text, 'html.parser')
-        links_list = []
-        for a in soup.find_all('a', href=True):
-                links_list.append(a['href'] + '\n')
-                
-        return links_list
-    except Exception as e:
-        log_exception(e)
-        print('Something went wrong. Check a logger for further information.')
-
-def get_correct_link(links_list):
-    pattern = re.compile(rf"/wiki/.*")
-    
-    for match in re.findall(pattern,str(links_list)):
-        print(match + '\n')
-        
-
-def find_paragraph(url):
-    pass
-
-def find_title(url):    
-    pass
-def get_link_from_parsed_website(links_file):
-    with open(links_file,'r') as reader:
-        pattern = re.compile(rf"/wiki/.+>")
-        pattern1 = re.compile(rf"language")
-        print(reader.read())
-
-
-        #matches = pattern.findall(str(link_list))
-        #for match in matches:
-        #    print(match)
 def log_exception(e):
     with open("logger.txt", 'a') as logger:
         logger.write(str(e) +  " at " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + '\n')
 
-#get_correct_link(parse_website(get_url()))
 
-#log_exception('bartek')
+def remove_duplicate(link_list):
+    link_list = list(set(link_list))
+    return link_list
+    
 
-a = parse_website(get_url())
-pattern = re.compile(rf"\A/wiki/")
-pattern1 = re.compile(rf"\A/wiki/.+:")
-pattern2 = re.compile(rf"\A/wiki/.+language")
-for i in a:
-    if pattern.search(i) :
-        if not pattern1.search(i):
-            if not pattern2.search(i):
-                print('we got hit: ' + 'https://en.wikipedia.org'+ i)
+def get_url_from_user(counter):
+    url = 'https://en.wikipedia.org/wiki/Asgardia'
+    print("What website would you like to parse? Would you like to start with Asgardia? If not just press 1 and paste your own website.:" + '\n')
+    if input() == str(1):
+        url = input('Paste your website')
+    links_list_level_0.append(url)
+    counter +=1
+    return url
+    
+def parse_website(url):
+    try:
+        content = requests.get(url)
+        soup = BeautifulSoup(content.text, 'html.parser')
+        return soup
+    except Exception as e:
+        log_exception(e)
+        print('Something went wrong with parsing. Check a logger for further information.')
+
+#def find_paragraph(soup):
+#    paragraph = soup.find('p')
+#    return paragraph.text
+
+def find_title(soup):    
+    title = soup.find('title')
+    return title.text
+
+def make_file(paragraph, title):
+    with open('file_with_data1.txt','a') as data_file:
+        data_file.write(': with a title as: ' + title + "and par as p: " + paragraph + '\n')
 
 
+def extract_links(soup):
+    try:
+        links_list = []
+        for a in soup.find_all('a', href=True):
+                links_list.append(a['href'])
+        return links_list
+    except Exception as e:
+        log_exception(e)
+        print("Soup file's problem. Check a logger for further information.")
+
+def get_correct_link(link_list):
+    a = link_list
+    pattern = re.compile(rf"\A/wiki/")
+    pattern1 = re.compile(rf"\A/wiki/(.+:)|(.+#)")
+    pattern2 = re.compile(rf"\A/wiki/.+language")
+    b = []
+    for i in a:
+        if pattern.search(i) :
+            if not pattern1.search(i):
+                if not pattern2.search(i):
+                    b.append('https://en.wikipedia.org'+ i)
+    return b
+
+def main_program(counter):
+    while counter < 2:
+        if len(links_list_level_0) == 0:
+            soup = parse_website(get_url_from_user(counter))
+            #paragraph = find_paragraph(soup)
+            #title = find_title(soup)
+            #make_file(paragraph,title)
+            links = extract_links(soup)
+            correct_links = get_correct_link(links)
+            correct_links = remove_duplicate(correct_links)
+            print(str(len(links_list_level_0))+' that is len of level 0.')
+            counter +=1
+        elif len(links_list_level_0) !=0 and len(links_list_level_1) == 0:
+            for url in correct_links:
+                soup = parse_website(url)
+                #paragraph = find_paragraph(soup)
+                #title = find_title(soup)
+                #make_file(paragraph,title)
+                links =extract_links(soup)
+                correct_links_level1 = get_correct_link(links)
+                correct_links_level1 = remove_duplicate(correct_links_level1)
+                for link in correct_links_level1:
+                    links_list_level_1.append(link)
+                with open('links1_after.txt', 'w') as links_file:
+                    for link in correct_links:
+                        links_file.write(link + '\n')
+            print(str(len(links_list_level_1)) + ' that is len level 1')
+            counter +=1
+        else:
+            for url in correct_links_level1:
+                soup = parse_website(url)
+                #paragraph = find_paragraph(soup)
+                #title = find_title(soup)
+                #make_file(paragraph,title)
+                links = extract_links(soup)
+                correct_links_level2 = get_correct_link(links)
+                correct_links_level2 = remove_duplicate(correct_links_level2)
+                for link in correct_links_level2:
+                    links_list_level_2.append(link)
+            counter +=1
+            with open('links_after.txt', 'w') as links_file:
+                for link in links_list_level_2:
+                    links_file.write(link + '\n')
+            print(str(len(links_list_level_2)) + ' that is len level 3')
+
+
+#find_paragraph(parse_website(get_url_from_user(0)))
+
+main_program(0)
 '''
-website = requests.get(url)
-soup = BeautifulSoup(website.text, 'html.parser')
-#print(soup)
-for a in soup.find_all('a', href=True):
-    with open('linki.txt','a') as f:
-        f.write(a['href'])
+a = find_title(parse_website(get_url_from_user(0)))
+print(type(a.text))
 
+a =['https://en.wikipedia.org/wiki/Syria','https://en.wikipedia.org/wiki/Syria','janina','janina']
 
-for paragraph in par_list:
-    print(paragraph.getText())
+b = remove_duplicate(a)
+print(b)
+#log_exception('bartek')
 '''
